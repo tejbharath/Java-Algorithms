c9c98529916c6ad2071225db9a409319ec07a882
Tej Bharath Mujje <tmujje@gmail.com>
2025-05-14 16:36:11 -0400

    Add PrefixSum Solutions for k Radius SubArray Avg sums problem

--- src/com/company/PrefixSum/KRadiusSubArrayAverages.java
+++ src/com/company/PrefixSum/KRadiusSubArrayAverages.java
@@ -1,40 +1,36 @@
 package com.company.PrefixSum;
 
+import java.util.Arrays;
+
 public class KRadiusSubArrayAverages {
     public int[] getAverages(int[] nums, int k) {
         int n = nums.length;
-        int elemCount = (2*k)+1;
-        int[] res = new int[n];
+        int elemCount = (2 * k) + 1;
 
-        if (elemCount > n)
+        if (k == 0)
         {
-            res[0] = -1;
+            return nums;
+        }
+
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+
+        if (elemCount > n) {
             return res;
         }
 
-        int[] prefix = new int[n];
+        long[] prefix = new long[n];
         prefix[0] = nums[0];
         for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + nums[i];
+            prefix[i] = prefix[i - 1] + nums[i];
         }
 
-        for (int j = 0; j < k ; j++) {
-            res[j] = -1;
-        }
-
-        for (int m = k; m < n-k ; m++) {
-            if (m != k)
-            {
-                res[m] = (prefix[m+k]-prefix[m-k])/ elemCount;
+        for (int m = k; m < n - k; m++) {
+            if (m != k) {
+                res[m] = (int) ((prefix[m + k] - prefix[m - k - 1]) / elemCount);
+            } else {
+                res[m] = (int) (prefix[m + k] / elemCount);
             }
-            else
-            {
-                res[m] = prefix[m+k]/ elemCount;
-            }
-        }
-
-        for (int l = n-k; l < n ; l++) {
-            res[l] = -1;
         }
 
         return res;
